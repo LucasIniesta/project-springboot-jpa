04e1646f1c44e15e755c6563bcb9f79232b5be09
Lucas Iniesta Simoes <l.iniesta.94@gmail.com>
2025-03-21 07:10:20 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/lucas/projeto_spring_jpa/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.projeto_spring_jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucas.projeto_spring_jpa.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
