a46fba7713ca1082b2e329b1baa37f2afd5215cf
Lucas Iniesta Simoes <l.iniesta.94@gmail.com>
2025-03-20 21:16:16 -0300

    Project Created

---
+++ src/main/java/com/lucas/projeto_spring_jpa/ProjetoSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.projeto_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/projeto_spring_jpa/ProjetoSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.projeto_spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
