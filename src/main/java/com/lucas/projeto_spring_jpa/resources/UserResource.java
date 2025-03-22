ba35b64442cc08148a2532e8b533c8a7c7fd9c98
Lucas Iniesta Simoes <l.iniesta.94@gmail.com>
2025-03-22 19:01:58 -0300

    Update user

--- src/main/java/com/lucas/projeto_spring_jpa/resources/UserResource.java
+++ src/main/java/com/lucas/projeto_spring_jpa/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import com.lucas.projeto_spring_jpa.entities.User;
@@ -47,5 +48,11 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = service.update(id, user);
+        return ResponseEntity.ok().body(user);
     }
 }
